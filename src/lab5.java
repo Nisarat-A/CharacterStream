5340718e66ef0ebce313397f65f796f18905485c
Nisarat-A <noansrnn@gmail.com>
2023-04-25 15:32:25 +0700

    -

--- src/lab5.java
+++ src/lab5.java
@@ -6,7 +6,19 @@ import java.util.logging.Logger;
 public class lab5 {
     public static void main(String[] args) {
    InputStreamReader in = new InputStreamReader(System.in);
+    BufferedReader input = new BufferedReader(in); //สร้าง obj class
 
+        try{
+            System.out.print("Enter your name: ");
+        String s = input.readLine();
+            System.out.println(s);
+            System.out.print("Enter your age: ");
+            String age = input.readLine();
+            System.out.println(age);
+            //buffer อ่านได้แค่ String
+        } catch (IOException e) {
+            Logger.getLogger(lab5.class.getName()).log(Level.SEVERE, null, e);
+        }
 
     }
 }
--- src/lab6.java
+++ src/lab6.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class lab6 {
+import java.util.Scanner;
+
+public class lab6 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String s = input.nextLine();
+        System.out.println("Your name is: " + s);
+        System.out.print("Enter your age: ");
+        int age = input.nextInt();
+        System.out.println("Your age is: " + age);
+        System.out.print("Enter your height: ");
+        double height = input.nextDouble();
+        System.out.println("Your height is: " + height);
+    }
 }
